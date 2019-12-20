ed296f8709a868ab10674c18be5af92f28d42b84
Arx1971 <adnan.rahin13@gmail.com>
2019-12-20 18:51:28 -0500

    Thread update

--- src/random_walk/GlobalField.java
+++ src/random_walk/GlobalField.java
@@ -3,7 +3,7 @@ package random_walk;
 public final class GlobalField {
 
 	static final int N_SAW = 100000;
-	static final int N_T = 1000;
+	static final int N_T = 10000;
 	static final int steps = 40;
 
 	static final int directions2d[][] = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
