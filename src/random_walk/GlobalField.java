cc756d6f5f22e5ff4677bbf57176ff5b24af9e78
Arx1971 <adnan.rahin13@gmail.com>
2020-10-25 15:24:42 -0400

    Number of Thread updates

--- src/random_walk/GlobalField.java
+++ src/random_walk/GlobalField.java
@@ -6,12 +6,12 @@ public final class GlobalField {
 	static final int N_T = 10000;
 	static final int steps = 40;
 
-	static final int directions2d[][] = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
+	static final int[][] directions2d = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
 
-	static final int directions3d[][] = { { 1, 0, 0 }, { -1, 0, 0 }, { 0, 1, 0 }, { 0, -1, 0 }, { 0, 0, 1 },
+	static final int[][] directions3d = { { 1, 0, 0 }, { -1, 0, 0 }, { 0, 1, 0 }, { 0, -1, 0 }, { 0, 0, 1 },
 			{ 0, 0, -1 } };
 
-	static final int directions4d[][] = { { 0, 0, 0, -1 }, { 0, 0, -1, 0 }, { 0, -1, 0, 0 }, { -1, 0, 0, 0 },
+	static final int[][] directions4d = { { 0, 0, 0, -1 }, { 0, 0, -1, 0 }, { 0, -1, 0, 0 }, { -1, 0, 0, 0 },
 			{ 0, 0, 0, 1 }, { 0, 0, 1, 0 }, { 0, 1, 0, 0 }, { 1, 0, 0, 0 } };
 
 }
